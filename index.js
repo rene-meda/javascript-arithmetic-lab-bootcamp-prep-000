83293a761ee9dd8f02dcf3def993878c34e0593a
Adolpho Noguera <adolph518@gmail.com>
2018-03-22 23:22:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+  return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+
+function dividie(a, b){
+  return a / b
+}
+
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
